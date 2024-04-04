30d195ceb65fcff73178e76e04c6cc962a0765ff
Sangam Subedi <87515320+sangamhoni@users.noreply.github.com>
2024-04-04 14:19:22 +0545

    Complete Exercise 2

---
+++ 05-javascript-basics/01-fundamentals/08-exercise-2/script.js
@@ -0,0 +1,16 @@
+/**
+ * The code below tells the browser to ask you for a number
+ * then if that number is `6`, it returns `true` otherwise it returns `false`
+ * 
+ * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ */
+
+number = Number(prompt("enter a number"));
+
+function numberChecker() {
+	if(number >= 10) {
+		return true;
+	} else {
+		return false;
+	}
+}
